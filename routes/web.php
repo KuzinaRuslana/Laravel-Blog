1cf1845c14b41d54897c0be5755702302b53fe23
Ruslana <kuzina.ruslana@yandex.ru>
2025-02-08 13:09:15 +0300

    Add index and show

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('about', [PageController::class, 'about']);
+
 Route::get('team', [PageController::class, 'team']);
+
+Route::get('articles', [ArticleController::class, 'index'])
+  ->name('articles.index');
+
+  Route::get('articles/{id}', [ArticleController::class, 'show'])
+  ->name('articles.show');
