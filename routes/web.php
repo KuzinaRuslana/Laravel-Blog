9df80d0499dca09ce29105f451b993895ea39c4f
Ruslana <kuzina.ruslana@yandex.ru>
2025-02-09 18:45:18 +0300

    Add create and store

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,11 @@ Route::get('team', [PageController::class, 'team']);
 Route::get('articles', [ArticleController::class, 'index'])
   ->name('articles.index');
 
-  Route::get('articles/{id}', [ArticleController::class, 'show'])
+Route::get('articles/create', [ArticleController::class, 'create'])
+  ->name('articles.create');
+
+Route::get('articles/{id}', [ArticleController::class, 'show'])
   ->name('articles.show');
+
+Route::post('articles', [ArticleController::class, 'store'])
+  ->name('articles.store');
