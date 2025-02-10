ad02e8b107bfbf0b897db5da723ce023f36b8d78
Ruslana <kuzina.ruslana@yandex.ru>
2025-02-10 19:06:00 +0300

    Add edit link

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,6 +5,7 @@
     @foreach ($articles as $article)
         <h2>
             <a href="{{ route('articles.show', $article) }}">{{ $article->name }}</a>
+            (<a href="{{ route('articles.edit', $article) }}">Edit</a>)
         </h2>
         <div>{{Str::limit($article->body, 200)}}</div>
     @endforeach
