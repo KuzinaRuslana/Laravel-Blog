b9eb66852ed36ce80bb1d8092f296c7bd2db9b93
Ruslana <kuzina.ruslana@yandex.ru>
2025-04-14 14:17:16 +0300

    Fix article.index.blade

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -10,20 +10,20 @@
                 <td>Действия</td>
             </tr>
         </thead>
-        </ttbod
-        @foreach ($articles as $article)
-            <tr>
-                <td><a class="text-decoration-none" href="{{ route('articles.show', $article) }}">{{ $article->name }}</a></td>
-                <td>
-                    <a class="text-decoration-none" href="{{route('articles.edit', $article->id)}}">Редактировать</a>
-                    <form action="{{ route('articles.destroy', $article->id) }}" method="POST" style="display: inline-block;" onsubmit="return confirm('Вы уверены?')">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-link text-danger p-0 m-0 align-baseline">Удалить</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
+        <tbody>
+            @foreach ($articles as $article)
+                <tr>
+                    <td><a class="text-decoration-none" href="{{ route('articles.show', $article) }}">{{ $article->name }}</a></td>
+                    <td>
+                        <a class="text-decoration-none" href="{{route('articles.edit', $article->id)}}">Редактировать</a>
+                        <form action="{{ route('articles.destroy', $article->id) }}" method="POST" style="display: inline-block;" onsubmit="return confirm('Вы уверены?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-link text-danger p-0 m-0 align-baseline">Удалить</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 @endsection
